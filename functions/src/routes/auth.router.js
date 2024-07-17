08c86b57c053e724c85185df26d390f4be1ab1ef
Joseph Ampadu <jkampadujnr@gmail.com>
2024-07-17 12:25:00 +0000

    sign up request body validation
    * install zod for schema validation
    * add validateRequestBody middleware before signup

--- functions/src/routes/auth.router.js
+++ functions/src/routes/auth.router.js
@@ -1,9 +1,15 @@
 const express = require("express");
 
 const { signUpWithEmailAndPassword } = require("../services/auth.service");
+const { validateRequestBody } = require("../middlewares/validation.middleware");
+const { EmailAndPasswordSignUpSchema } = require("../schemas/auth.schema");
 
 const router = express.Router();
 
-router.post("/signup", signUpWithEmailAndPassword);
+router.post(
+  "/signup",
+  validateRequestBody(EmailAndPasswordSignUpSchema),
+  signUpWithEmailAndPassword
+);
 
 module.exports = router;
