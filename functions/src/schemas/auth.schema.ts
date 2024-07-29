019196e5bc4eb6788ca6617905997e6fb8a2775e
Joseph Ampadu <jkampadujnr@gmail.com>
2024-07-29 20:07:50 +0000

    transform zod error to API error

--- functions/src/schemas/auth.schema.ts
+++ functions/src/schemas/auth.schema.ts
@@ -1,12 +1,23 @@
+/* eslint-disable object-curly-spacing */
 import * as z from "zod";
 
 export const EmailAndPasswordSignUpSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(8),
-  displayName: z.string(),
+  email: z
+    .string({ message: "Email is required" })
+    .email({ message: "Email must be a valid email" }),
+  password: z
+    .string({ message: "Password is required" })
+    .min(8, "Password must be at least 8 chars long"),
+  displayName: z
+    .string({ message: "Display name is required" })
+    .min(2, "Display name must be at least 2 chars long"),
 });
 
 export const EmailAndPasswordLogInSchema = z.object({
-  email: z.string().email(),
-  password: z.string(),
+  email: z
+    .string({ message: "Email is required" })
+    .email({ message: "Email must be a valid email" }),
+  password: z
+    .string({ message: "Password is required" })
+    .min(1, "Password is required"),
 });
--- functions/src/schemas/posts.schema.ts
+++ functions/src/schemas/posts.schema.ts
@@ -1,13 +1,26 @@
+/* eslint-disable object-curly-spacing */
 import * as z from "zod";
 
 export const CreateBlogPostSchema = z.object({
-  title: z.string().min(1),
-  description: z.string().min(1),
-  body: z.string().min(1),
+  title: z
+    .string({ message: "Title is required" })
+    .min(1, "Title must at least 1 char long"),
+  description: z
+    .string({ message: "Description is required" })
+    .min(1, "Description must be at least 1 char long"),
+  body: z
+    .string({ message: "Body is required" })
+    .min(1, "Body must be at least 1 char long"),
 });
 
 export const UpdateBlogPostSchema = z.object({
-  title: z.string().min(1),
-  description: z.string().min(1),
-  body: z.string().min(1),
+  title: z
+    .string({ message: "Title is required" })
+    .min(1, "Title must at least 1 char long"),
+  description: z
+    .string({ message: "Description is required" })
+    .min(1, "Description must be at least 1 char long"),
+  body: z
+    .string({ message: "Body is required" })
+    .min(1, "Body must be at least 1 char long"),
 });
